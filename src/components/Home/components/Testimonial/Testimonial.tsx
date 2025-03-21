fd093d38c69d688ebe9564bcb972bc441ee68538
Golam Rabbani <golamrabbani9373@gmail.com>
2025-03-21 11:48:27 +0600

    testimonials data create complete

--- src/components/Home/components/Testimonial/Testimonial.tsx
+++ src/components/Home/components/Testimonial/Testimonial.tsx
@@ -1,49 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import {Marquee} from '@/components/magicui/marquee';
+import {testimonials} from '@/data/testimonials';
 import React from 'react';
-
-const testimonials = [
-	{
-		name: 'Lena Torres',
-		role: 'Product Lead • Creatify',
-		image: 'https://randomuser.me/api/portraits/women/32.jpg',
-		title: 'A frontend developer who transforms visions into reality',
-		text: 'Aayush has an exceptional ability to bring our design concepts to life. His attention to detail and dedication to creating seamless user experiences have greatly enhanced our projects.',
-		gradient: 'bg-gradient-to-b from-pink-300 to-gray-500 text-black',
-	},
-	{
-		name: 'Emma Thompson',
-		role: 'Senior Designer • PixelCraft',
-		image: 'https://randomuser.me/api/portraits/women/42.jpg',
-		title: 'Efficient, innovative, and a pleasure to work with',
-		text: 'Working with Aayush has been a fantastic experience. He’s not only highly skilled in frontend technologies but also brings a refreshing creativity to every project.',
-		gradient: 'bg-gradient-to-b from-blue-400 to-blue-800 text-white',
-	},
-	{
-		name: 'Ethan Parker',
-		role: 'Manager • TechFusion',
-		image: 'https://randomuser.me/api/portraits/men/46.jpg',
-		title: 'A reliable developer with a keen eye for detail',
-		text: 'Aayush delivered exceptional work on our project. His meticulous approach and strong communication skills greatly enhanced the quality of our web applications.',
-		gradient: 'bg-gradient-to-b from-purple-600 to-gray-900 text-white',
-	},
-	{
-		name: 'Ryan Mitchell',
-		role: 'CTO • Innovate Solutions',
-		image: 'https://randomuser.me/api/portraits/men/78.jpg',
-		title: 'Consistent excellence in every project',
-		text: 'Aayush is a standout frontend developer. His ability to deliver high-quality code and engaging user interfaces consistently makes him a valuable team member.',
-		gradient: 'bg-gradient-to-b from-teal-600 to-teal-900 text-white',
-	},
-	{
-		name: 'Emily Thompson',
-		role: 'Creative Director • H. Studios',
-		image: 'https://randomuser.me/api/portraits/women/97.jpg',
-		title: 'A master of frontend development with a keen design sensibility',
-		text: 'Aayush is a fantastic frontend developer! He took our requirements and turned them into something amazing.',
-		gradient: 'bg-gradient-to-b from-green-600 to-green-900 text-white',
-	},
-];
 
 const Testimonials = () => {
 	return (
