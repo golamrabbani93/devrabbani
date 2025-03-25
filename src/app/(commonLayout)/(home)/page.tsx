e369670e3c14c79950b2a9d57f172ce720e193a3
Golam Rabbani <golamrabbani9373@gmail.com>
2025-03-25 10:51:20 +0600

    create projects page components

--- src/app/(commonLayout)/(home)/page.tsx
+++ src/app/(commonLayout)/(home)/page.tsx
@@ -1,4 +1,4 @@
-import HomeComponent from '@/components/Home';
+import HomeComponent from '@/components/pages/Home';
 
 export default function Home() {
 	return (
--- src/app/(commonLayout)/projects/page.tsx
+++ src/app/(commonLayout)/projects/page.tsx
@@ -1,5 +1,10 @@
+import Project from '@/components/pages/Project';
 const page = () => {
-	return <div>This is page component</div>;
+	return (
+		<div>
+			<Project />
+		</div>
+	);
 };
 
 export default page;
