a19f069c4927e4ba156b85010b493b98b3f0ef81
Golam Rabbani <golamrabbani9373@gmail.com>
2025-03-25 10:44:08 +0600

    change projects iterface

--- src/types/projects.interface.ts
+++ src/types/projects.interface.ts
@@ -1,30 +1,25 @@
 export interface IProjects {
 	id: string;
 	name: string;
-	image: string;
-	description: string;
-	live_link: string;
-	admin_info: AdminInfo;
-	highLightTech: Technology[];
-	github_link: GithubLink[];
-	technology: Technology[];
-	feature: string;
-	site_images: string[];
+	fullPhoto: string;
+	images: string[];
+	liveLink: string;
+	githubClientLink: string;
+	githubServerLink?: string;
+	technologies?: Technology[];
+	features: string[];
 	filter: string;
-	item: string;
-}
-
-export interface AdminInfo {
-	email: string;
-	pass: string;
-}
-
-export interface GithubLink {
-	client_side: string;
-	server_side?: string;
+	isDeleted: boolean;
+	adminEmail?: string;
+	adminPassword?: string;
+	status: string;
+	createdAt?: string;
+	updatedAt?: string;
+	__v?: number;
 }
 
 export interface Technology {
+	_id?: string;
 	name: string;
 	image: string;
 }
