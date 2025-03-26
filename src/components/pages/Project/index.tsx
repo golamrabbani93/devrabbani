08e3f3b9c48f938705c1acf679c9359f1ac6236b
Golam Rabbani <golamrabbani9373@gmail.com>
2025-03-26 11:46:21 +0600

    show only dynamic data

--- src/components/pages/Project/index.tsx
+++ src/components/pages/Project/index.tsx
@@ -3,11 +3,12 @@ import BreadCrumb from './components/BreadCrumb/BreadCrumb';
 import {projectsData} from '@/data/projects';
 
 const index = () => {
+	const data = projectsData.filter((project) => project.filter === 'dynamic');
 	return (
 		<>
 			<BreadCrumb />
 			<section className="relative mx-auto w-full max-w-7xl mt-0 px-4 py-36">
-				<Projects projects={projectsData} more={false} />
+				<Projects projects={data} more={false} />
 			</section>
 		</>
 	);
