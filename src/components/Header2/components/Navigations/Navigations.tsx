aac453dfec83e234e6a4301698cb4bcd805455af
Golam Rabbani <golamrabbani9373@gmail.com>
2025-04-05 08:18:38 +0600

    open contact drawer when click on contact me

--- src/components/Header2/components/Navigations/Navigations.tsx
+++ src/components/Header2/components/Navigations/Navigations.tsx
@@ -197,40 +197,23 @@ const Navigations = () => {
 			title: 'Bucket List',
 			description: 'Things to do at least once in my life',
 		},
-		// {
-		// 	id: 'book-a-call',
-		// 	icon: (
-		// 		<svg
-		// 			xmlns="http://www.w3.org/2000/svg"
-		// 			width="24"
-		// 			height="24"
-		// 			viewBox="0 0 24 24"
-		// 			fill="none"
-		// 			stroke="currentColor"
-		// 			strokeWidth="2"
-		// 			strokeLinecap="round"
-		// 			strokeLinejoin="round"
-		// 			className={`lucide lucide-phone size-5 text-neutral-200 ${
-		// 				pathname === '/book-a-call' ? 'text-white' : 'group-hover:text-black'
-		// 			}`}
-		// 		>
-		// 			<path d="M16 2v2"></path>
-		// 			<path d="M7 22v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"></path>
-		// 			<path d="M8 2v2"></path>
-		// 			<circle cx="12" cy="11" r="3"></circle>
-		// 			<rect x="3" y="4" width="18" height="18" rx="2"></rect>
-		// 		</svg>
-		// 	),
-		// 	title: 'Book a Call',
-		// 	description: 'Book a call with me',
-		// },
 	];
+	//close modal when click on navigation item
 	const modalClose = () => {
 		const closeButton = document.getElementById('close-dialog') as HTMLButtonElement;
 		if (closeButton) {
 			closeButton.click();
 		}
 	};
+	//open contact drawer when click on contact me
+	const contactDrawer = () => {
+		modalClose();
+		const contactButton = document.getElementById('contact-drawer') as HTMLButtonElement;
+		if (contactButton) {
+			contactButton.click();
+		}
+	};
+
 	return (
 		<div
 			data-slot="command-group"
@@ -270,6 +253,46 @@ const Navigations = () => {
 						</div>
 					</Link>
 				))}
+				<div
+					data-slot="command-item"
+					className="data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative cursor-pointer gap-2.5 rounded-xl px-2 py-0.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 group flex items-start  hover:bg-white/10 hover:text-white/90"
+					cmdk-item=""
+					role="option"
+					aria-disabled="false"
+					aria-selected="false"
+					data-disabled="false"
+					data-selected="false"
+					onClick={() => contactDrawer()}
+				>
+					<div className={`mt-0.5 rounded-lg  p-2 group-hover:bg-white/80`}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							className={`lucide lucide-phone size-5 text-neutral-200 ${
+								pathname === '/book-a-call' ? 'text-white' : 'group-hover:text-black'
+							}`}
+						>
+							<path d="M16 2v2"></path>
+							<path d="M7 22v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"></path>
+							<path d="M8 2v2"></path>
+							<circle cx="12" cy="11" r="3"></circle>
+							<rect x="3" y="4" width="18" height="18" rx="2"></rect>
+						</svg>
+					</div>
+					<div className="flex flex-col">
+						<span className="line-clamp-1 text-sm">Contact Me</span>
+						<span className="mt-0.5 line-clamp-1 text-xs text-neutral-400 group-hover:text-white">
+							Book a call with me
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
