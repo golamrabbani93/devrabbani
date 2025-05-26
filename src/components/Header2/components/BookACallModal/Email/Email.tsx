63bc5310358cb64205cf3ff8512cf3779d6eb323
Golam Rabbani <golamrabbani9373@gmail.com>
2025-05-26 18:13:32 +0600

    add email in modal section

--- src/components/Header2/components/BookACallModal/Email/Email.tsx
+++ src/components/Header2/components/BookACallModal/Email/Email.tsx
@@ -15,14 +15,13 @@ const Email = () => {
 				devrabbani9@gmail.com
 			</Link>
 
-			<a
-				href="/cal"
+			<span
 				aria-label="Book a Call"
 				className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0 cursor-pointer border border-input shadow-xs hover:text-accent-foreground h-9 px-4 py-2 has-[>svg]:px-3 w-full bg-zinc-800/40 font-light text-white hover:bg-zinc-700/50"
 			>
 				<Calendar className="mr-2 size-5" />
 				Book a Call
-			</a>
+			</span>
 		</div>
 	);
 };
