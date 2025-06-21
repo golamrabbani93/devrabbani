3bbc8d7bd4db1bf9b83e16ab886ffe43f58136f7
Golam Rabbani <golamrabbani9373@gmail.com>
2025-06-21 19:23:21 +0600

    add meta tag in projects page

--- src/app/(commonLayout)/projects/page.tsx
+++ src/app/(commonLayout)/projects/page.tsx
@@ -1,4 +1,11 @@
 import Project from '@/components/pages/Project';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+	title: 'Works || DevRabbani',
+	description:
+		'Explore the projects and works of DevRabbani, showcasing web development skills and innovative solutions.',
+};
 const page = () => {
 	return (
 		<div>
