d81b27ddb5fe85b8cfc41e84f3473e2b57304173
Golam Rabbani <golamrabbani9373@gmail.com>
2025-06-21 19:22:37 +0600

    add meta tag in about page

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,4 +1,11 @@
 import About from '@/components/pages/About';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+	title: 'About || DevRabbani',
+	description:
+		'Learn more about DevRabbani, the mission, vision, and the team behind the web development tutorials and tech insights.',
+};
 const page = () => {
 	return (
 		<>
