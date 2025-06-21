74fa785887c9c2a3ffc07464b1afe2eda3181784
Golam Rabbani <golamrabbani9373@gmail.com>
2025-06-21 19:22:59 +0600

    add meta tag in blog page

--- src/app/(commonLayout)/blog/page.tsx
+++ src/app/(commonLayout)/blog/page.tsx
@@ -1,5 +1,10 @@
 import Blog from '@/components/pages/Blog';
+import {Metadata} from 'next';
 
+export const metadata: Metadata = {
+	title: 'Blogs || DevRabbani',
+	description: 'Read the latest blogs on web development, programming, and technology.',
+};
 const page = () => {
 	return (
 		<>
