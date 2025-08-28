256b4d23b296dd4625c21bfe8ff43f82b2a105db
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-28 15:40:13 +0600

    modify single projects

--- src/components/pages/SingleProjects/SingleProjects.tsx
+++ src/components/pages/SingleProjects/SingleProjects.tsx
@@ -1,10 +1,13 @@
-import Breadcrumb from './Breadcrumb/Breadcrumb';
+import Article from './components/Article/Article';
+import Breadcrumb from './components/Breadcrumb/Breadcrumb';
 
 const SingleProjects = () => {
 	return (
 		<main className="relative mx-auto w-full">
 			{/* Breadcrumb component here */}
 			<Breadcrumb />
+			<div className="pt-60"></div>
+			<Article />
 		</main>
 	);
 };
