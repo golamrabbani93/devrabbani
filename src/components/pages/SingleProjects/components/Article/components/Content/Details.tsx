5bde9698e18991fefbb0daa25d425ae335074f9b
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-30 01:05:00 +0600

    update content image section

--- src/components/pages/SingleProjects/components/Article/components/Content/Details.tsx
+++ src/components/pages/SingleProjects/components/Article/components/Content/Details.tsx
@@ -5,28 +5,31 @@ import Footer from './Footer';
 import Overview from './Overview';
 import TechStack from './TechStack';
 import Outcome from './Outcome';
-
+import './Content.scss';
 /* eslint-disable @next/next/no-img-element */
 const Details = ({project}: {project: IProject}) => {
 	return (
 		<div className="prose">
 			<div>
 				<div className="relative overflow-hidden rounded-2xl border border-white/15 bg-[#f2f2f20c] p-1.5 shadow-2xl md:p-2">
-					<span aria-owns="rmiz-modal-f6d8cb589b2c" data-rmiz="">
-						<span data-rmiz-content="found" style={{visibility: 'visible'}}>
-							<img
-								alt="Next Venture screenshot"
-								loading="lazy"
-								width="1203"
-								height="753"
-								decoding="async"
-								className="!my-0 rounded-[8px] lg:my-0"
-								sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 900px"
-								src="/images/project-breadcrumb.png"
-								style={{color: 'transparent'}}
-							/>
-						</span>
-					</span>
+					{/* Image Section */}
+					<div className="pointer-events-none relative cursor-pointer overflow-hidden rounded-2xl  shadow-2xl md:pointer-events-auto lg:h-[500px]">
+						<div
+							className="absolute inset-x-0 top-0 h-px"
+							style={{
+								background:
+									'linear-gradient(90deg, rgba(0, 0, 0, 0) 5%, rgba(255, 255, 255, 0.8) 35%, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.8) 65%, rgba(0, 0, 0, 0) 95%)',
+							}}
+						></div>
+						<div className="group relative flex size-full flex-col items-center justify-between overflow-hidden rounded-xl lg:rounded-2xl bg-gradient-to-b from-black to-transparent transition-all duration-100 z-20">
+							<div
+								className="item w-full  h-[300px] lg:h-full bg-cover bg-center bg-no-repeat"
+								style={{
+									backgroundImage: `url(${project?.fullPhoto})`,
+								}}
+							></div>
+						</div>
+					</div>
 				</div>
 			</div>
 
