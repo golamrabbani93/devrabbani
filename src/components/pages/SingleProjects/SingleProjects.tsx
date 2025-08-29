0e88bc2a35d49e4a4f56fd0c783f6f429cdab37a
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-30 00:18:34 +0600

    props project to chaild component

--- src/components/pages/SingleProjects/SingleProjects.tsx
+++ src/components/pages/SingleProjects/SingleProjects.tsx
@@ -1,13 +1,22 @@
+import {allProjects} from '@/data/AllProjects';
 import Article from './components/Article/Article';
 import Breadcrumb from './components/Breadcrumb/Breadcrumb';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
-const SingleProjects = () => {
+const SingleProjects = ({slug}: {slug: string}) => {
+	const project = allProjects.find((proj) => proj.name.toLowerCase() === slug);
+
+	// return to not found page
+	if (!project) {
+		return <NotFoundPage />;
+	}
+
 	return (
 		<main className="relative mx-auto w-full">
 			{/* Breadcrumb component here */}
 			<Breadcrumb />
 			<div className="pt-60"></div>
-			<Article />
+			<Article project={project} />
 		</main>
 	);
 };
