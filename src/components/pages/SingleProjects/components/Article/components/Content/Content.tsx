94c975d06464ec90ec7e00d6dbf8e81db901b2c9
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-30 00:20:08 +0600

    add new project interface

--- src/components/pages/SingleProjects/components/Article/components/Content/Content.tsx
+++ src/components/pages/SingleProjects/components/Article/components/Content/Content.tsx
@@ -1,10 +1,8 @@
 import Header from './Header';
 import Details from './Details';
-import {projects} from '@/data/newProjects';
+import {IProject} from '@/types/projects.interface';
 
-const Content = () => {
-	const project = projects[0];
-	console.log('🚀🚀 ~ Details ~ project:', project);
+const Content = ({project}: {project: IProject}) => {
 	return (
 		<article className="flex min-w-0 w-full flex-col gap-4 px-4 pt-8 md:px-6 md:mx-auto xl:pt-12 xl:px-12 prose-lg prose-a:underline-offset-4 prose-a:decoration-blue-500">
 			{/* HEADER */}
