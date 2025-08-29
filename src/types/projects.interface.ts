e072ca30949da76f79cd21c5053fb53c9595961c
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-30 00:21:16 +0600

    remove previous project interface

--- src/types/projects.interface.ts
+++ src/types/projects.interface.ts
@@ -1,25 +1,3 @@
-export interface IProjects {
-	id: string;
-	name: string;
-	fullPhoto: string;
-	images: string[];
-	liveLink: string;
-	description?: string;
-	githubClientLink: string;
-	githubServerLink?: string;
-	technologies?: Technology[];
-	features: string[];
-	filter: string;
-	isDeleted: boolean;
-	adminEmail?: string;
-	adminPassword?: string;
-	highLightTech?: {name: string; image: string}[];
-	status: string;
-	createdAt?: string;
-	updatedAt?: string;
-	__v?: number;
-}
-
 export interface Technology {
 	_id?: string;
 	name: string;
@@ -64,6 +42,7 @@ export interface IProject {
 	fullPhoto: string;
 	images: string[];
 	liveLink: string;
+	features: string[];
 	githubClientLink: string;
 	highLightTech: IHighLightTech[];
 	description: string;
@@ -72,4 +51,11 @@ export interface IProject {
 	keyFeatures: IKeyFeatures[];
 	color: string;
 	technologies: ITech[];
+	isDeleted: boolean;
+	adminEmail?: string;
+	adminPassword?: string;
+	status: string;
+	createdAt?: string;
+	updatedAt?: string;
+	__v?: number;
 }
