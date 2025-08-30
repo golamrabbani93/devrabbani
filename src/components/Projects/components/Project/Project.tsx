c1e07161a4668f6c82269de334b41a5de2d9a1e7
Golam Rabbani <golamrabbani9373@gmail.com>
2025-08-30 16:34:12 +0600

    add responsive view components

--- src/components/Projects/components/Project/Project.tsx
+++ src/components/Projects/components/Project/Project.tsx
@@ -3,6 +3,7 @@ import MotionFollower from '../MotionFollow/MotionFollow';
 import {motion} from 'framer-motion';
 import './Project.scss';
 import Link from 'next/link';
+import ResponsiveView from './ResponsiveView';
 interface ProjectProps {
 	id: string;
 	project: IProject;
@@ -38,72 +39,7 @@ const Project = ({project, id}: ProjectProps) => {
 						></div>
 					</div>
 				</Link>
-				<div className="mt-6 mb-24 flex flex-col px-2 lg:hidden">
-					<div className="flex items-center">
-						<h2 className="my-auto line-clamp-1 text-lg font-bold">{project?.name}</h2>
-						<div aria-hidden="true" className="ml-3 h-1 min-w-6 rounded-full bg-pink-600"></div>
-					</div>
-					<p className="text-muted-foreground mt-2 text-sm">{project?.description}</p>
-					<div className="my-4 flex max-w-fit flex-wrap items-center gap-1">
-						{project?.highLightTech?.map((tech, index) => (
-							<div
-								key={index}
-								className="inline rounded-sm border-white/[0.34] px-1.5 py-[3px] text-xs fill-pink-600 text-pink-400 bg-pink-600/20 lg:bg-black"
-							>
-								{tech.name}
-							</div>
-						))}
-					</div>
-					<div className="flex w-full gap-4">
-						<a
-							href={project?.githubClientLink}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="relative flex h-fit w-1/2 items-center justify-center gap-2 rounded-xl bg-neutral-900 px-5 py-2 shadow-[inset_0_2px_10px_#ffffff1f]"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="lucide lucide-github size-4"
-							>
-								<path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-								<path d="M9 18c-4.51 2-5-2-7-2"></path>
-							</svg>
-							Code
-						</a>
-						<a
-							href={project?.liveLink}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="relative flex h-fit w-1/2 items-center justify-center gap-2 rounded-xl bg-neutral-900 px-5 py-2 shadow-[inset_0_2px_10px_#ffffff1f] after:inset-0 after:bg-black/50"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								className="lucide lucide-external-link size-4"
-							>
-								<path d="M15 3h6v6"></path>
-								<path d="M10 14 21 3"></path>
-								<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-							</svg>
-							Website
-						</a>
-					</div>
-				</div>
+				<ResponsiveView project={project} />
 			</div>
 			<MotionFollower id={id} />
 		</motion.div>
